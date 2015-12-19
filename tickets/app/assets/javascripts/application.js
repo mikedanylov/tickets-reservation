c464911bd2c0b7120932f47087e7ae9c1c30ba5f
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-19 17:41:51 +0200

    Added Navbar and Login modal

--- tickets/app/assets/javascripts/application.js
+++ tickets/app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+  // Login icon toggles modal
+  $('#login').click(function () {
+    $('#loginModal').modal('show');
+  });
+  
+});
