aba4e2668b1899dc8a4bbdbff7a0ea78d1e9c158
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-19 16:10:21 +0200

    Put row numbers and seat images in a table

--- tickets/app/controllers/seats_controller.rb
+++ tickets/app/controllers/seats_controller.rb
@@ -1,6 +1,6 @@
 class SeatsController < ApplicationController
   def index
-    n_rows = Seat.all.pluck('DISTINCT row')
+    n_rows = Seat.all.order(row: :desc).pluck('DISTINCT row')
     @seats_arr = []
     n_rows.each do |row_num|
       @seats_arr.push(Seat.all.order(:number).where(row: row_num).to_a)
