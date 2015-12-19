bfc84741c402dc2833f170007e2e8f1d9e514e38
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-19 21:44:50 +0200

    Mostly sessions#new styles

--- tickets/app/controllers/sessions_controller.rb
+++ tickets/app/controllers/sessions_controller.rb
@@ -8,14 +8,17 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(password)
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Logged in successfully!"
+      flash.notice = "Logged in successfully!"
+      redirect_to root_path
     else
-      redirect_to login_path, alert: "Invalid email or password!"
+      flash.alert = "Invalid email or password!"
+      redirect_to login_path
     end
   end
 
   def destroy
     reset_session
-    redirect_to root_path, notice: "You have been logged out"
+    flash.notice = "You have been logged out"
+    redirect_to root_path
   end
 end
