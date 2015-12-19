31ca2d9229d2504d9014858b234996266cca480e
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-19 15:06:14 +0200

    Initial seats#index controller method and view

--- tickets/app/controllers/seats_controller.rb
+++ tickets/app/controllers/seats_controller.rb
@@ -1,5 +1,9 @@
 class SeatsController < ApplicationController
   def index
-    @seats = Seat.all
+    n_rows = Seat.all.pluck('DISTINCT row')
+    @seats_arr = []
+    n_rows.each do |row_num|
+      @seats_arr.push(Seat.all.order(:number).where(row: row_num).to_a)
+    end
   end
 end
