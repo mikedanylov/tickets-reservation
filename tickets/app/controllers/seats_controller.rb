f3d61fbd39ac4f236733bf31fcd0a7ac3749d5c1
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-20 14:16:49 +0200

    Minor fixes

--- tickets/app/controllers/seats_controller.rb
+++ tickets/app/controllers/seats_controller.rb
@@ -6,16 +6,11 @@ class SeatsController < ApplicationController
     n_rows.each do |row_num|
       @seats_arr.push(Seat.all.order(:number).where(row: row_num).to_a)
     end
-
-    if logged_in?
-      print "\n\n"
-      print current_user
-      print "\n\n"
-    end
-
   end
 
   def update
     # get input from form filled by admin and make changes to db
+    flash.notice = "Seats status is updated!"
+    redirect_to root_path
   end
 end
