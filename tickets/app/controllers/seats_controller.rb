ee193ad057caa3d50fd546e37c808043e0074ab6
Mykhailo Danylov <mike.danylov@gmail.com>
2015-12-21 12:38:57 +0200

    Implemented seats#update method

--- tickets/app/controllers/seats_controller.rb
+++ tickets/app/controllers/seats_controller.rb
@@ -9,10 +9,12 @@ class SeatsController < ApplicationController
   end
 
   def update
-    print "\n\n"
     json_obj = JSON.parse(params[:seats_to_update])
-    print json_obj
-    print "\n\n"
+    json_obj.each do |obj|
+      seat_to_update = Seat.find_by(row: obj['row'], number: obj['number'])
+      seat_to_update.status = obj['status']
+      seat_to_update.save!
+    end
     # get input from form filled by admin and make changes to db
     flash.notice = "Seats status is updated!"
     redirect_to root_path
