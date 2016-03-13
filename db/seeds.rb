69a6216971413152b5b9aec55ed89ffba7c78d81
Mykhailo Danylov <mike.danylov@gmail.com>
2016-03-13 22:29:23 +0200

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,30 @@
 # Seat.destroy_all
 # User.destroy_all
 
-# for row in 1..11
-#   case row
-#   when 1
-#     for seat in 1..17
-#       Seat.create!(row: row, number: seat, status: stat.sample, price: 10)
-#     end
-#   when 2, 3, 8, 9
-#     for seat in 1..18
-#       Seat.create!(row: row, number: seat, status: stat.sample, price: 10)
-#     end
-#   when 4, 5, 6, 7
-#     for seat in 1..14
-#       Seat.create!(row: row, number: seat, status: stat.sample, price: 10)
-#     end
-#   when 10, 11
-#     for seat in 1..9
-#       Seat.create!(row: row, number: seat, status: stat.sample, price: 10)
-#     end
-#   end
-# end
+for row in 1..11
+  case row
+  when 1, 8
+    for seat in 1..17
+      # Seat.create!(row: row, number: seat, status: stat.sample, price: 10)
+      Seat.update_attribute('price', 15)
+    end
+  when 2, 3, 9
+    for seat in 1..18
+      # Seat.create!(row: row, number: seat, status: stat.sample, price: 10)
+      Seat.update_attribute('price', 15)
+    end
+  when 4, 5, 6, 7
+    for seat in 1..14
+      # Seat.create!(row: row, number: seat, status: stat.sample, price: 10)
+      Seat.update_attribute('price', 15)
+    end
+  when 10, 11
+    for seat in 1..9
+      # Seat.create!(row: row, number: seat, status: stat.sample, price: 10)
+      Seat.update_attribute('price', 15)
+    end
+  end
+end
 
 # admin = User.create!(email: ENV["ADMIN_EMAIL"], password: ENV["ADMIN_PWD"], is_admin: true)
 # someguy = User.create!(email: 'some@example.com', password: '123qwe', is_admin: false)
@@ -31,5 +35,3 @@
 #   seat.user = admin
 #   seat.save!
 # end
-
-Seat.where(row: 8, number: 18).destroy_all
